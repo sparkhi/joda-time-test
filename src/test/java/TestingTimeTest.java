8c6b247b908e666fb7690cfc49f8b3f8aee2c1aa
sparkhi <parkhi@gmail.com>
2024-01-25 10:16:16 +0000

    only ubuntu

--- src/test/java/TestingTimeTest.java
+++ src/test/java/TestingTimeTest.java
@@ -8,6 +8,17 @@ public class TestingTimeTest {
     public void shouldConvertLongMillisToFormattedDate() {
         String dateString = new TestingTime().getDateString(12345678L);
         assertEquals("1970-01-01T04:25:45", dateString);
+    }
 
+    @Test
+    public void shouldConvertJavaDateToFormattedDate() {
+        String dateString = new TestingTime().getDateStringJavaUtilOnly(12345678L);
+        assertEquals("1970-01-01T04:25:45", dateString);
+    }
+
+    @Test
+    public void shouldHaveSameTimeInMillisAsTheInstantToJodaTime() {
+        long instant = 12345678L;
+        assertEquals(instant, new TestingTime().getConvertedInstant(instant));
     }
 }
